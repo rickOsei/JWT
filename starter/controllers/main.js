1b003eea26eabda20cb5dba73dfa56b60b479060
Osei Rick <rickoseiampah@gmail.com>
2023-01-19 16:10:09 +0300

    first commit

---
+++ starter/controllers/main.js
@@ -0,0 +1,32 @@
+// check username,password in post(login) request
+// if exist create new JWT
+// send back to front-end
+// setup authentication so only the request with JWT can access the dashboard
+const jwt = require("jsonwebtoken");
+const { BadRequestError } = require("../errors");
+
+const login = async (req, res) => {
+  const { username, password } = req.body;
+  // mongoose validation
+  // Joi
+  // check in the controller
+  if (!username || !password) {
+    throw new BadRequestError("Please provide email and password");
+  }
+
+  const id = new Date().getTime();
+  const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+  res.status(200).json({ msg: "user created", token });
+};
+
+const dashboard = async (req, res) => {
+  const luckyNumber = Math.floor(Math.random() * 100);
+  res.status(200).json({
+    msg: `Hello, ${req.user.username}`,
+    secret: `Here is your authorized data, your lucky number is ${luckyNumber}`,
+  });
+};
+
+module.exports = { login, dashboard };
